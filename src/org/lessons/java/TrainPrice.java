5ab60ebc455964e69deaf9cbec5dd74f896d20cf
VincenzoTavassi <info@egadivacanze.it>
2023-05-31 15:06:40 +0200

    add variables

--- src/org/lessons/java/TrainPrice.java
+++ src/org/lessons/java/TrainPrice.java
@@ -6,19 +6,22 @@ public class TrainPrice {
 
     public static void main(String[] args) {
 
+        double pricePerKm = 0.21;
+        double seniorDiscount = 0.60;
+        double juniorDiscount = 0.80;
+
         Scanner Scanner = new Scanner(System.in);
         System.out.println("Inserisci la distanza in km da percorrere");
         int distance = Scanner.nextInt();
         System.out.println("Inserisci la tua età");
         int userAge = Scanner.nextInt();
 
-        double price = distance * 0.21;
+        double price = distance * pricePerKm;
         if (userAge > 65) {
-            price = price * 0.60;
+            price = price * seniorDiscount;
         } else if(userAge < 18) {
-            price = price * 0.80;
+            price = price * juniorDiscount;
         }
-
 
         String finalPrice = new DecimalFormat("#########.##").format(price);
         System.out.println("Il prezzo per te è di € " + finalPrice);
